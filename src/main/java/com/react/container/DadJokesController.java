7bf24b96571cedf81794ae153c5f50b269e0edea
mayur ingle <inglemayur8890@gmail.com>
2020-03-24 19:22:14 +0530

    first commit

---
+++ src/main/java/com/react/container/DadJokesController.java
@@ -0,0 +1,15 @@
+package com.react.container;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class DadJokesController {
+    @GetMapping("/api/dadjokes")
+    public String dadJokes() {
+        return "Justice is a dish best served cold, if it were served warm it would be just water.";
+    }
+}
+
+
+
